3ff3a0a2b863faa17c0c5f5785b92111dd9adf7b
Abhilash Nair <abhilashnair@MacBook-Pro.domain>
2023-04-21 13:19:34 -0600

    replaced it with dispatch switch statement that allows reducer to have multiple switch statements

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,21 +4,30 @@
 import React, {useReducer} from 'react'
 
 // newState and action are interchangeable
-const countReducer = (state, action) => ({...state,...(typeof action === 'function' ? action(state) : action)})
+// const countReducer = (state, action) => ({...state,...(typeof action === 'function' ? action(state) : action)})
   // callback setCount will update based on what is passed in
   // state is an object along with state
   // any property the action has which is defined by setState will overwride any property of the state
   // function based
   //determine if function or object, return action called on state if function or return iterable
   
-
+function countReducer(state, action) {
+  switch(action.type) {
+    case 'INCREMENT': {
+      return {count: state.count + action.step}
+    }
+    default: {
+      break
+    }
+  }
+}
 
 
 
 function Counter({initialCount = 0, step = 1}) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
-  const [state, setState] = useReducer(countReducer,{
+  const [state, dispatch] = useReducer(countReducer,{
     count: initialCount
   })
 
@@ -27,8 +36,8 @@ function Counter({initialCount = 0, step = 1}) {
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  //const increment = () => setState(currentState => ({count: count + step}))
-  const increment = () => setState({count: count + step})
+  //const increment = () => dispatch(currentState => ({count: count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
