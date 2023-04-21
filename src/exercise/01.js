de19ea28ffc88a02b87bb1d7a20cdc3fb04f633c
Abhilash Nair <abhilashnair@MacBook-Pro.domain>
2023-04-21 13:01:52 -0600

    useReducer to simulate state and setState

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,25 +3,30 @@
 
 import React, {useReducer} from 'react'
 
-const initialCount = 0
-
-function countReducer(count, newCount) {
+// newState and action are interchangeable
+function countReducer(state, action) {
   // callback setCount will update based on what is passed in
-  return newCount
+  // state is an object along with state
+  console.log(state, action)
+  // any property the action has which is defined by setState will overwride any property of the state
+  return ({...state, ...action})
 }
 
 
 
-function Counter({step = 1}) {
+function Counter({initialCount = 0, step = 1}) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = useReducer(countReducer,initialCount)
+  const [state, setState] = useReducer(countReducer,{
+    count: initialCount
+  })
 
+  const {count} = state
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+  const increment = () => setState({count: count + step})
   return <button onClick={increment}>{count}</button>
 }
 
