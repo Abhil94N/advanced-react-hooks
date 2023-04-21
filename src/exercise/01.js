6248e121b1638e9e1a9af94fc913ba1fc8baf4e4
Abhilash Nair <abhilashnair@MacBook-Pro.domain>
2023-04-21 13:20:55 -0600

    throw error in default dispatch action

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -17,7 +17,7 @@ function countReducer(state, action) {
       return {count: state.count + action.step}
     }
     default: {
-      break
+      throw new Error(`Unsupported action type: ${action.type}`)
     }
   }
 }
